cd519c94ea37f188c89d736866851c65e0d29e27
Bluemi <bruno.schilling@protonmail.ch>
2017-06-07 08:28:07 +0100

    implemented hello World in VS2017Enterprise Test commit

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KiBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World");
+            Console.ReadKey();
+        }
+    }
+}

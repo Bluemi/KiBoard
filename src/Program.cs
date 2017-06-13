6d5a5cc90d93537165e32e2a6fc3766342d37605
Bluemi <bruno.schilling@protonmail.ch>
2017-06-13 14:44:33 +0100

    +Calibrator(interface); +SpaceTranslator(interface);+PointBuffer; +Vec3D; +CalibrationPoint not implemented yet

--- src/Program.cs
+++ src/Program.cs
@@ -7,8 +7,7 @@ namespace KiBoard
         static void Main(string[] args)
         {
             Console.WriteLine("hello World");
-            Console.WriteLine("3");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
